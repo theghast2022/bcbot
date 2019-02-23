eff2f60c058958068de65cf0230efb9c4f0a81f0
agasjgabasfas <44522450+theghast2022@users.noreply.github.com>
2019-02-23 14:06:23 +0100

    Update shard.js

--- shard.js
+++ shard.js
@@ -485,4 +485,23 @@ message.guild.members.filter(m => m.presence.status === 'online').forEach(m => {
      })
  })
     }});
+
+
+client.on('message', message => {
+    if (message.content === ('/bot')) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .addField('**Bot Ping**🚀 :' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('**Servers**📚 :', [client.guilds.size], true)
+            .addField('**Channels**📝 :' , `[ ${client.channels.size} ]` , true)
+            .addField('**Users**🔮 :' ,`[ ${client.users.size} ]` , true)
+            .addField('**Bot Name**🔰 :' , `[ ${client.user.tag} ]` , true)
+            .addField('**Bot Owner**👑 :' , `[<@388357326614167563>]` , true)
+            .setFooter(message.author.username, message.author.avatarURL)
+    })
+}
+});
 client.login(process.env.BOT_TOKEN)
